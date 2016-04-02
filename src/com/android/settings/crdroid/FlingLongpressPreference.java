ResurrectionRemix/Resurrection_packages_apps_Settings

[SQUASHED] Settings : Add DU Fling,Pulse,SmartBar
Action Picker Dialog :

http://gerrit.dirtyunicorns.com/23916

Author:Randall Rushing <randall.rushing@gmail.com>

Expose Navbar for External Fragment Launching

http://gerrit.dirtyunicorns.com/23914

Author:Randall Rushing <randall.rushing@gmail.com>

Settings: Support Bringup

http://gerrit.dirtyunicorns.com/24717

Author:Randall Rushing <randall.rushing@gmail.com>

Revert old options :-
SLim Dim
CM navbar
Slim Navbar Dimensions

Signed-off-by: Varun Date <date.varun123@gmail.com>

src/com/android/settings/rr/FlingLongpressPreference.java
@@ -1,0 +1,197 @@
+/*
+ * Copyright (C) 2014 TeamEos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.rr;
+
+import com.android.settings.R;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.UserHandle;
+import android.preference.DialogPreference;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class FlingLongpressPreference extends DialogPreference implements
+        SeekBar.OnSeekBarChangeListener {
+    private static final String TAG = FlingLongpressPreference.class.getSimpleName();
+
+    private static final String SETTINGSNS = "http://schemas.android.com/apk/res/com.android.settings";
+    private static final String ATTR_URI = "observedUri";
+    private String mActionUri;
+
+    // just a hair about tap timeout
+    private static final int MIN_VAL = 25;
+    // 500, but everything shifts by 25
+    private static final int MAX_VAL = ViewConfiguration.getLongPressTimeout() - MIN_VAL;
+
+    private SeekBar mLongPress;
+    private ContentResolver mResolver;
+
+    // what settings provider holds
+    private int mStoredVal;
+
+    // track the instance state of the seekbar
+    private int mTempVal;
+
+    public FlingLongpressPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+
+        // this was hard coded for NX, but we can use for anything that
+        // takes a longpress timeout configuration. So try to load
+        // the uri from attr, if not, it's NX
+        int uriRes = attrs.getAttributeResourceValue(SETTINGSNS, ATTR_URI, -1);
+        mActionUri = uriRes != -1 ? context.getResources().getString(uriRes) : Settings.Secure.FLING_LONGPRESS_TIMEOUT;
+
+        mStoredVal = Settings.Secure.getIntForUser(mResolver, mActionUri, MAX_VAL, UserHandle.USER_CURRENT);
+        mTempVal = mStoredVal - MIN_VAL;
+        setDialogLayoutResource(R.layout.fling_longpress_timeout);
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+        ViewGroup container = (ViewGroup) view
+                .findViewById(R.id.fling_longpress_container);
+        mLongPress = (SeekBar) container.findViewById(R.id.seekbar);
+        mLongPress.setMax(MAX_VAL);
+        mLongPress.setProgress(mStoredVal - MIN_VAL);
+        mLongPress.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+        builder.setNeutralButton(R.string.long_swipe_reset,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        AlertDialog d = (AlertDialog) getDialog();
+        Button defaultsButton = d.getButton(DialogInterface.BUTTON_NEUTRAL);
+        defaultsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLongPress.setProgress(MAX_VAL);
+            }
+        });
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+            boolean fromUser) {
+        // always holds seekbar value, not adjusted settings value
+        mTempVal = progress;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (!positiveResult) {
+            // user canceled, set Temp values back to
+            // last saved settings vals
+            mTempVal = mStoredVal - MIN_VAL;
+            return;
+        }
+        mStoredVal = mTempVal + MIN_VAL;
+        Settings.Secure.putIntForUser(mResolver, mActionUri, mStoredVal, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        final Parcelable superState = super.onSaveInstanceState();
+        if (getDialog() == null || !getDialog().isShowing()) {
+            return superState;
+        }
+
+        // Save the dialog state
+        final SavedState myState = new SavedState(superState);
+        myState.mLongpressState = mTempVal;
+
+        return myState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state == null || !state.getClass().equals(SavedState.class)) {
+            // Didn't save state for us in onSaveInstanceState
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        SavedState myState = (SavedState) state;
+        super.onRestoreInstanceState(myState.getSuperState());
+
+        mLongPress.setProgress(myState.mLongpressState);
+    }
+
+    private static class SavedState extends BaseSavedState {
+        int mLongpressState;
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        public SavedState(Parcel source) {
+            super(source);
+            mLongpressState = source.readInt();
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+    }
+}

src/com/android/settings/rr/IconPickHelper.java
@@ -1,0 +1,80 @@
+/**
+ * Copyright (C) 2016 The DirtyUnicorns Project
+ * Author: Randall Rushing <randall.rushing@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Wrapper class for delivering icon picker results to activities or fragments
+ */
+
+package com.android.settings.rr;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+
+public class IconPickHelper {
+    public static final int REQUEST_PICK_ICON_PACK = 100;
+    public static final int REQUEST_PICK_ICON_GALLERY = 101;
+    public static final String INTENT_ACTION_EDIT_CLASS = "com.android.settings";
+    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.android.settings.rr.IconPickerActivity";
+    private static final int REQUEST_CODE = 420;
+    private Activity mParent;
+    private OnPickListener mListener;
+    private int lastFragmentId;
+    private int mIconPickType;
+
+    public interface OnPickListener {
+        void iconPicked(String iconType, String iconPackage, String iconName);
+    }
+
+    public IconPickHelper(Activity parent, OnPickListener listener) {
+        mParent = parent;
+        mListener = listener;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE:
+                    String iconType = data.getStringExtra("icon_data_type");
+                    String iconPackage = data.getStringExtra("icon_data_package");
+                    String iconName = data.getStringExtra("icon_data_name");
+                    mListener.iconPicked(iconType, iconPackage, iconName);
+                    break;
+            }
+        }
+    }
+
+    public void pickIcon(int fragmentId, int requestType) {
+        lastFragmentId = fragmentId;
+        mIconPickType = requestType;
+        if (requestType == REQUEST_PICK_ICON_PACK) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.setClassName(INTENT_ACTION_EDIT_CLASS, INTENT_ACTION_ICON_PICKER_COMPONENT);
+            startFragmentOrActivity(intent, REQUEST_CODE);
+        }
+    }
+
+    private void startFragmentOrActivity(Intent pickIntent, int requestCode) {
+        if (lastFragmentId == 0) {
+            mParent.startActivityForResult(pickIntent, requestCode);
+        } else {
+            Fragment cFrag = mParent.getFragmentManager().findFragmentById(lastFragmentId);
+            if (cFrag != null) {
+                mParent.startActivityFromFragment(cFrag, pickIntent, requestCode);
+            }
+        }
+    }
+}

VlikeBanana/android_packages_apps_Settings-7

Conflicts:
	AndroidManifest.xml
	res/drawable/fab_accent.xml
	res/values-es-rUS/cm_plurals.xml
	res/values/cr_colors.xml
	res/values/rr_arrays.xml
	res/values/rr_strings.xml
	res/xml/button_settings.xml
	res/xml/navbar_dimensions.xml
	res/xml/rr_main_settings.xml
	src/com/android/settings/ButtonSettings.java
	src/com/android/settings/crdroid/NavBarDimensions.java

src/com/android/settings/crdroid/FlingLongpressPreference.java
@@ -1,0 +1,197 @@
+/*
+ * Copyright (C) 2014 TeamEos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.crdroid;
+
+import com.android.settings.R;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.UserHandle;
+import android.preference.DialogPreference;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class FlingLongpressPreference extends DialogPreference implements
+        SeekBar.OnSeekBarChangeListener {
+    private static final String TAG = FlingLongpressPreference.class.getSimpleName();
+
+    private static final String SETTINGSNS = "http://schemas.android.com/apk/res/com.android.settings";
+    private static final String ATTR_URI = "observedUri";
+    private String mActionUri;
+
+    // just a hair about tap timeout
+    private static final int MIN_VAL = 25;
+    // 500, but everything shifts by 25
+    private static final int MAX_VAL = ViewConfiguration.getLongPressTimeout() - MIN_VAL;
+
+    private SeekBar mLongPress;
+    private ContentResolver mResolver;
+
+    // what settings provider holds
+    private int mStoredVal;
+
+    // track the instance state of the seekbar
+    private int mTempVal;
+
+    public FlingLongpressPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+
+        // this was hard coded for NX, but we can use for anything that
+        // takes a longpress timeout configuration. So try to load
+        // the uri from attr, if not, it's NX
+        int uriRes = attrs.getAttributeResourceValue(SETTINGSNS, ATTR_URI, -1);
+        mActionUri = uriRes != -1 ? context.getResources().getString(uriRes) : Settings.Secure.FLING_LONGPRESS_TIMEOUT;
+
+        mStoredVal = Settings.Secure.getIntForUser(mResolver, mActionUri, MAX_VAL, UserHandle.USER_CURRENT);
+        mTempVal = mStoredVal - MIN_VAL;
+        setDialogLayoutResource(R.layout.fling_longpress_timeout);
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+        ViewGroup container = (ViewGroup) view
+                .findViewById(R.id.fling_longpress_container);
+        mLongPress = (SeekBar) container.findViewById(R.id.seekbar);
+        mLongPress.setMax(MAX_VAL);
+        mLongPress.setProgress(mStoredVal - MIN_VAL);
+        mLongPress.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+        builder.setNeutralButton(R.string.long_swipe_reset,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        AlertDialog d = (AlertDialog) getDialog();
+        Button defaultsButton = d.getButton(DialogInterface.BUTTON_NEUTRAL);
+        defaultsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLongPress.setProgress(MAX_VAL);
+            }
+        });
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+            boolean fromUser) {
+        // always holds seekbar value, not adjusted settings value
+        mTempVal = progress;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (!positiveResult) {
+            // user canceled, set Temp values back to
+            // last saved settings vals
+            mTempVal = mStoredVal - MIN_VAL;
+            return;
+        }
+        mStoredVal = mTempVal + MIN_VAL;
+        Settings.Secure.putIntForUser(mResolver, mActionUri, mStoredVal, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        final Parcelable superState = super.onSaveInstanceState();
+        if (getDialog() == null || !getDialog().isShowing()) {
+            return superState;
+        }
+
+        // Save the dialog state
+        final SavedState myState = new SavedState(superState);
+        myState.mLongpressState = mTempVal;
+
+        return myState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state == null || !state.getClass().equals(SavedState.class)) {
+            // Didn't save state for us in onSaveInstanceState
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        SavedState myState = (SavedState) state;
+        super.onRestoreInstanceState(myState.getSuperState());
+
+        mLongPress.setProgress(myState.mLongpressState);
+    }
+
+    private static class SavedState extends BaseSavedState {
+        int mLongpressState;
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        public SavedState(Parcel source) {
+            super(source);
+            mLongpressState = source.readInt();
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+    }
+}

src/com/android/settings/crdroid/IconPickHelper.java
@@ -1,0 +1,80 @@
+/**
+ * Copyright (C) 2016 The DirtyUnicorns Project
+ * Author: Randall Rushing <randall.rushing@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Wrapper class for delivering icon picker results to activities or fragments
+ */
+
+package com.android.settings.crdroid;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+
+public class IconPickHelper {
+    public static final int REQUEST_PICK_ICON_PACK = 100;
+    public static final int REQUEST_PICK_ICON_GALLERY = 101;
+    public static final String INTENT_ACTION_EDIT_CLASS = "com.android.settings";
+    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.android.settings.crdroid.IconPickerActivity";
+    private static final int REQUEST_CODE = 420;
+    private Activity mParent;
+    private OnPickListener mListener;
+    private int lastFragmentId;
+    private int mIconPickType;
+
+    public interface OnPickListener {
+        void iconPicked(String iconType, String iconPackage, String iconName);
+    }
+
+    public IconPickHelper(Activity parent, OnPickListener listener) {
+        mParent = parent;
+        mListener = listener;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE:
+                    String iconType = data.getStringExtra("icon_data_type");
+                    String iconPackage = data.getStringExtra("icon_data_package");
+                    String iconName = data.getStringExtra("icon_data_name");
+                    mListener.iconPicked(iconType, iconPackage, iconName);
+                    break;
+            }
+        }
+    }
+
+    public void pickIcon(int fragmentId, int requestType) {
+        lastFragmentId = fragmentId;
+        mIconPickType = requestType;
+        if (requestType == REQUEST_PICK_ICON_PACK) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.setClassName(INTENT_ACTION_EDIT_CLASS, INTENT_ACTION_ICON_PICKER_COMPONENT);
+            startFragmentOrActivity(intent, REQUEST_CODE);
+        }
+    }
+
+    private void startFragmentOrActivity(Intent pickIntent, int requestCode) {
+        if (lastFragmentId == 0) {
+            mParent.startActivityForResult(pickIntent, requestCode);
+        } else {
+            Fragment cFrag = mParent.getFragmentManager().findFragmentById(lastFragmentId);
+            if (cFrag != null) {
+                mParent.startActivityFromFragment(cFrag, pickIntent, requestCode);
+            }
+        }
+    }
+}